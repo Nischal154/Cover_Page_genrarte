3bbf22cdb16e8df96c9ea09eb392d203018b880e
Nischal Pandey <nischalpandey27@gmail.com>
2026-04-01 23:27:51 +0530

    first commit

---
+++ app.mjs
@@ -0,0 +1,40 @@
+import express from "express";
+import fs from "fs/promises";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+//middleware
+app.use(express.urlencoded({ extended: true }));  
+app.use(express.static("public"));
+//view engine
+app.set("view engine", "ejs");
+
+const port = 8080;
+
+app.get("/form", (req, res) => {
+    res.render("form");
+});
+
+app.get("/preview", async (req, res) => {
+    try {
+        const countFilePath = path.join(__dirname, "user_count.json");
+        const fileData = await fs.readFile(countFilePath, "utf8");
+        const countData = JSON.parse(fileData);
+        countData.count += 1;
+        await fs.writeFile(countFilePath, JSON.stringify(countData, null, 2));
+
+        res.render("preview", { ...req.query, count: countData.count });
+    } catch(err) {
+        console.error("Error updating count:", err);
+        res.status(500).send("Server Error");
+    }
+});
+
+app.listen(port, () => {
+    console.log(`application is listening on ${port}`);
+});
